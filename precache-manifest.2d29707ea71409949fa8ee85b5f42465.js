52997b142c0e0acb0364aa085dbde12bbba9012d
Nelio Santos <nelio.santos@jbtec.com.br>
2020-02-01 10:30:57 -0300

    Updates

---
+++ precache-manifest.2d29707ea71409949fa8ee85b5f42465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e1fa5518179ad4cc078b85bd4ff055",
+    "url": "/binarylab/index.html"
+  },
+  {
+    "revision": "8e274dbcca035b6be574",
+    "url": "/binarylab/static/css/main.238b5aaf.chunk.css"
+  },
+  {
+    "revision": "35786f87a496634c75f3",
+    "url": "/binarylab/static/js/2.68d96ad1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/binarylab/static/js/2.68d96ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e274dbcca035b6be574",
+    "url": "/binarylab/static/js/main.3ba8e94e.chunk.js"
+  },
+  {
+    "revision": "37f1c23eb3245b8f6669",
+    "url": "/binarylab/static/js/runtime-main.20724579.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04761e7c6ff4a6de9149730c570a55cd.js"
+  "/binarylab/precache-manifest.2d29707ea71409949fa8ee85b5f42465.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/binarylab/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
