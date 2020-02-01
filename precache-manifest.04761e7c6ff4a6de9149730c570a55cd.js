9b4ed9cae49ad0df5db5b2f3a27b4b831fb60acd
Nelio Santos <nelio.santos@jbtec.com.br>
2020-02-01 10:22:07 -0300

    Updates

---
+++ precache-manifest.04761e7c6ff4a6de9149730c570a55cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab85463a84298c56c19df12e4388712",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e274dbcca035b6be574",
+    "url": "/static/css/main.238b5aaf.chunk.css"
+  },
+  {
+    "revision": "35786f87a496634c75f3",
+    "url": "/static/js/2.68d96ad1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.68d96ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e274dbcca035b6be574",
+    "url": "/static/js/main.3ba8e94e.chunk.js"
+  },
+  {
+    "revision": "54a78e1d2a8a53039fd6",
+    "url": "/static/js/runtime-main.a5228786.js"
+  }
+]);
