2ce919f1ac41bce7d25de17ffbdb834c7a5cfce7
Nelio Santos <nelio.santos@jbtec.com.br>
2020-02-02 11:47:29 -0300

    Updates

---
+++ precache-manifest.1db2494fd6a3368bc9014bc1bf0261cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb06ea9b460c80a5a5858c358a0ebbec",
+    "url": "/binarylab/index.html"
+  },
+  {
+    "revision": "f1bc601b08471af997f6",
+    "url": "/binarylab/static/css/main.90ae94db.chunk.css"
+  },
+  {
+    "revision": "d886972bc755cca32135",
+    "url": "/binarylab/static/js/2.0e10b1da.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/binarylab/static/js/2.0e10b1da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1bc601b08471af997f6",
+    "url": "/binarylab/static/js/main.05cd63af.chunk.js"
+  },
+  {
+    "revision": "37f1c23eb3245b8f6669",
+    "url": "/binarylab/static/js/runtime-main.20724579.js"
+  }
+]);
